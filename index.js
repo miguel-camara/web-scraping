22d9abd5540476040045a920cd6a6da5c3b8145d
miguel-camara <miguelordonezcamara@hotmail.com>
2023-03-31 15:12:24 -0600

    update

--- index.js
+++ index.js
@@ -1,48 +1,59 @@
 import fetch from 'node-fetch';
 import { load } from 'cheerio';
 import pretty from 'pretty';
+import axios from 'axios';
 
 import fs from 'fs';
 
 const url = 'https://www.itescam.edu.mx/portal/avisos.php';
 
-const response = await fetch(url);
+// Axios
+const response = await axios.get(url, {
+  responseType: 'arraybuffer',
+  responseEncoding: 'binary',
+});
+const body = new TextDecoder('iso-8859-1').decode(response.data);
 
-const body = await response.text();
-
-// Para visualizar el HTML
-// console.log( pretty(body) );
+// Fetch API
+// const response = await fetch(url);
+// const buffer = await response.arrayBuffer();
+// const body = new TextDecoder('iso-8859-1').decoder.decode(buffer);
 
 let $ = load(body);
 
-const list = $(".panel-warning").children(".panel-body").children("h5");
+const list = $('.panel-warning').children('.panel-body').children('h5');
 
 const warnings = [];
 
 list.each((index, element) => {
+  const title = $(element).children('a').children('font').text();
+  const link = `https://www.itescam.edu.mx/portal/${$(element)
+    .children('a')
+    .attr('href')}`;
+  const result = $(element)
+    .children('small')
+    .text()
+    .split(' || ')
+    .map(item => item.trim());
 
-    const title = $(element).children("a").children("font").text();
-    const link = $(element).children("a").attr("href");
-    const split = $(element).children("small").text();
-    
-    const info = split.split(" || ");
+  // Destructuring
+  let [staff, department, date, expiration, view] = result;
 
-    const warning = {
-        title: title,
-        staff: info[0].trim(),
-        department: info[1].trim(),
-        date: info[2].trim(),
-        expiration: info[3].trim(),
-        view: info[4].trim(),
-        link: `https://www.itescam.edu.mx/portal/${link}`
-    }
-    
-    warnings.push(warning);
+  const warning = {
+    title,
+    staff,
+    department,
+    date,
+    expiration,
+    view,
+    link,
+  };
+
+  warnings.push(warning);
 });
 
 // Guardar en un archivo
-fs.writeFile('warnings.json', JSON.stringify(warnings, null,2), (err) => {
-
-    if (err) throw err;
-    console.log('The file has been saved!');
+fs.writeFile('warnings.json', JSON.stringify(warnings, null, 2), err => {
+  if (err) throw err;
+  console.log('The file has been saved!');
 });
